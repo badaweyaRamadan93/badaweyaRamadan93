96b017104a7d0ee805fccd740813e8cbb21976e7
badaweya ramadan <badaweya.ramadan93@gmail.com>
2020-01-21 22:03:47 -0800

    first projsct in github

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$("document").ready(function(){
+    $("#inform").click(function(){
+        $(".detali").slideToggle();
+    });
+    $("#summ").click(function(){
+        $(".persSum").slideToggle();
+    });
+    $("#education").click(function(){
+        $(".edu-detalis").slideToggle();
+    });
+    $("#training").click(function(){
+        $(".train-deta").slideToggle();
+    });
+    $("#tickinckal").click(function(){
+        $(".tickinickal-skils").slideToggle();
+    });
+    $("#pers").click(function(){
+        $(".pers-skils").slideToggle();
+    });
+    
+});
